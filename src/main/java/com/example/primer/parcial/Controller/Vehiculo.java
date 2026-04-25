61e845f6286b236bbaa4404a64befaef003af876
Dafne-Tapia <romidafne23@gmail.com>
2026-04-24 21:58:08 -0400

    revisado codigo

--- src/main/java/com/example/primer/parcial/Controller/Vehiculo.java
+++ src/main/java/com/example/primer/parcial/Controller/Vehiculo.java
@@ -19,7 +19,7 @@ public class Vehiculo {
         this.placa = placa;
         this.color = color;
     }
-    // revisado el codigo, hola mundo.
+    // hola mundo, pase a revisarlo.
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
